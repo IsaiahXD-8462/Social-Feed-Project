34f8e14886bfd591ee7a41a31d7f9b98b4e7e1e3
IsaiahXD-8462 <116024098+IsaiahXD-8462@users.noreply.github.com>
2023-04-19 14:34:11 -0400

    Made a jsx file for adding and submitting new posts

---
+++ social-feed/src/components/Add Post Entry/AddPostEntryForm.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+
+const AddPostEntryForm = (props) => {
+    
+    const [post, makePost] = useState('');
+    const [date, setDate] = useState('');
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        let newPost = {
+            post: makePost,
+            date: setDate
+        };
+        console.log(newPost);
+        props.addNewEntryProperty(addNewPost)
+    }
+
+    return ( 
+        <form onSubmit={handleSubmit}>
+            <label>Post</label>
+            <input type='text' value={post} onChange={(event) => makePost(event.target.value)} />
+            <label>Date</label> 
+            <input type='date' value={date}onChange={(event) => setDate(event.target.value)} />
+            <button type='submit'>Add</button>
+        </form>
+     );
+}
+ 
+export default AddPostEntryForm;
